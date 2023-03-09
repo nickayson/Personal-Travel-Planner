f7429223917001ea86d467697224ae2f3ccac46f
nickayson <aysonnicholas@gmail.com>
2023-03-08 18:38:42 -0800

    restart

---
+++ personal-travel-planner/src/App.js
@@ -0,0 +1,11 @@
+import TripList from "./components/TripList";
+
+function App() {
+  return (
+    <div className="container">
+      <TripList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ personal-travel-planner/src/components/TripList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class TripList extends React.Component {
+    render() {
+        return( 
+            <div>This will be the Trips Listed</div>
+        );
+    }
+}
