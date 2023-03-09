69e11c5ee8adcf43788e6abcc83f8d11e4c65d23
rishubg <rishub.goel@gmail.com>
2023-03-08 19:22:54 -0800

    Added events

--- personal-travel-planner/src/App.js
+++ personal-travel-planner/src/App.js
@@ -1,9 +1,11 @@
 import TripList from "./components/TripList";
+import Events from "./components/Events";
 
 function App() {
   return (
     <div className="container">
       <TripList/>
+      <Events/>
     </div>
   );
 }
