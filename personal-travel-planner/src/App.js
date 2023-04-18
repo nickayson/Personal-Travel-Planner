acdc7c6c4f92545017f23d43ba4150d7546c79bb
rishubg <rishub.goel@gmail.com>
2023-04-17 19:30:27 -0700

    Added more world clock times however theres an error

--- personal-travel-planner/src/App.js
+++ personal-travel-planner/src/App.js
@@ -21,10 +21,14 @@ function App() {
         <WorldClock timezone="India/Delhi" label="Delhi" />
         <WorldClock timezone="Philippines/Manila" label="Manila" />
         <WorldClock timezone="America/New_York" label="New_York" />
-        <WorldClock timezone="Philippines/Manila" label="Manila" />
-        <WorldClock timezone="Philippines/Manila" label="Manila" />
-        <WorldClock timezone="Philippines/Manila" label="Manila" />
-        <WorldClock timezone="Philippines/Manila" label="Manila" />
+        <WorldClock timezone="America/Los_Angeles" label="Los_Angeles" />
+        <WorldClock timezone="Europe/London" label="London" />
+        <WorldClock timezone="Europe/Berlin" label="Berlin" />
+        <WorldClock timezone="Asia/Shanghai" label="Shanghai" />
+        <WorldClock timezone="Australia/Sydney" label="Sydney" />
+        <WorldClock timezone="Africa/Johannesburg" label="Johannesburg" />
+        <WorldClock timezone="America/Sao_Paulo" label="Sao_Paulo" />
+        <WorldClock timezone="Asia/Dubai" label="Dubai" />
       </div>
     </div>
     
